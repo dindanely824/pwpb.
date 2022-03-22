d0ebec7313d7771e7d61d35fd39be45193f94930
dindanely824 <93692803+dindanely824@users.noreply.github.com>
2022-03-22 13:14:19 +0700

    Delete ujikom_dinda directory

--- ujikom_dinda/admin/config/Config.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-
-define('BASEURL','http://localhost/ujikom_dinda/public/');
--- ujikom_dinda/admin/core/Controller.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-class Controller{
-    public function view($view, $data=[]){
-        require_once '../admin/views/'. $view. '.php';
-    }
-}
